00b420af63b4d5084d3adf6274e200e7d1b5a5ee
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-28 06:15:02 -0900

    Update main_page.dart

--- poem/lib/screens/main_page.dart
+++ poem/lib/screens/main_page.dart
@@ -90,6 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
   //   }
   // }
 
+  
   Future _getImage() async {
     final pickedImage =
         await ImagePicker().pickImage(source: ImageSource.gallery);
